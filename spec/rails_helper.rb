cf7c1f4094c42c53b2d927a2252c5ba17cd1db82
krishnapal <krishnapal.g@greenbotz.co>
2025-12-26 18:04:18 +0530

    Setup RSpec with FactoryBot and DatabaseCleaner

---
+++ spec/rails_helper.rb
@@ -0,0 +1,26 @@
+# spec/rails_helper.rb - Add at the bottom
+
+RSpec.configure do |config|
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
+
+  # Database Cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  # Shoulda Matchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+end
